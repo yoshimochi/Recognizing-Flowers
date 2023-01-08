532b29ca7acbbdb98c5de07cfa75ac06f1bf2c7d
yoshimochi <olive.oil6319@gmail.com>
2023-01-08 23:49:52 +0900

    学習・推論ためのコード生成

--- flower/recognition/model.py
+++ flower/recognition/model.py
@@ -6,8 +6,9 @@ def set_parameter_requires_grad(model, feature_extracting):
         for param in model.parameters():
             param.requires_grad = False
 
-def initialoze_model(num_classes, feature_extract=False, use_pretrained=True):
+def initialize_model(num_classes, feature_extract=False, use_pretrained=True):
     model_ft = models.resnet18(pretrained=use_pretrained)
     set_parameter_requires_grad(model_ft, feature_extract)
     num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, num_classes)
     return model_ft
