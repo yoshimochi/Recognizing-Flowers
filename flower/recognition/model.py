e69e01f1fcf042ae8a29837cdaef9572fc05744a
yoshimochi <olive.oil6319@gmail.com>
2023-01-08 22:59:21 +0900

    深層学習モデルの作成

---
+++ flower/recognition/model.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+from torchvision import models
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+def initialoze_model(num_classes, feature_extract=False, use_pretrained=True):
+    model_ft = models.resnet18(pretrained=use_pretrained)
+    set_parameter_requires_grad(model_ft, feature_extract)
+    num_ftrs = model_ft.fc.in_features
+    return model_ft
